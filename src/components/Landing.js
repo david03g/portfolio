4bf576bd153f62040342967c99162ddc036425b0
david03g <dgoldstein.us@gmail.com>
2023-02-24 13:30:19 -0800

    links

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,10 +5,10 @@ import Avatar from '@mui/material/Avatar';
 import david from '../art/david.jpg';
 import icon from '../art/icon.png';
 import olem from '../art/olem.png';
-
+import EmailIcon from '@mui/icons-material/Email';
 import ampedchess from '../art/ampedchess.gif';
 import ordermedia from '../art/ordermedia.jpg';
-
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import ucb from '../art/ucb.jpg';
 
 import { makeStyles } from "@material-ui/core";
@@ -51,25 +51,23 @@ export default function Landing() {
 
             <div style={{marginBottom:0, marginTop:20, maxWidth:900, width:'100%', display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center'}}>
             
-                        <Avatar  sx={{ width: 150, height: 150 }} style={{marginRight:15}} src={david} />
+                <Avatar   sx={{ width: 150, height: 150 }} src={david} />
 
-                    
+                <div style={{marginTop:20, display:'flex', flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+                    {/* <EmailIcon style={{color:'white', marginRight:10}}/> */}
+                    <p style={{padding:0, margin:0, color:'white', fontWeight:'bold'}}>davidg3@protonmail.com</p>
+                </div>
 
-              
-
+                <div style={{display:'flex', marginTop:20, flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+                    {/* <LocalPhoneIcon style={{color:'white', marginRight:10}}/> */}
+                    <p style={{padding:0, margin:0, color:'white', fontWeight:'bold'}}>+1 (305) 775-2267</p>
+                </div>
 
             </div>
 
 
+            <div style={{marginTop:20, maxWidth:900, width:'100%'}}>
 
-         
-         
-
-
-            <div style={{marginTop:50, maxWidth:900, width:'100%'}}>
-
-
-           
 
                 <div className={classes.container}>
                     <div className={classes.border} />
@@ -77,19 +75,19 @@ export default function Landing() {
                         }</span>
                     <div className={classes.border} />
                 </div>
-
          
 
                 <div class='profile-card' style={{display:'flex', flexDirection:'column', backgroundColor:'#081424', borderRadius:10, padding:20}}>
                     <div style={{display:'flex', flexDirection:'row', alignItems:'center',  borderRadius:10}} >
                         <Avatar   sx={{ width: 80, height: 80 }} src={ampedchess} />
                         <div style={{display:'flex', marginLeft:15, flexDirection:'column', justifyContent:'flexStart'}}>
-                            <p style={{fontWeight:'normal', color:'white', fontSize:16, }} ><b>Amped Chess LLC</b> <br/> Win money playing chess <br/> Esports</p>
+                            <p style={{fontWeight:'normal', color:'white', fontSize:16, }} ><b>Amped Chess</b> <br/> Win money playing chess <br/> Esports</p>
                         </div>
                     </div>
                         <p style={{color:'white', fontSize:14}}>
                         Both professional sports and esports encounter a common phenomenon known as the "tournament effect," where only a small number of exceptionally skilled or entertaining players can monetize their participation in the sport. Unfortunately, this leaves millions of everyday players with few or no options to compete for prizes. Amped Chess seeks to address this issue by creating a more lucrative environment for the game of chess, primarily through prize pool tournaments and wagers. Currently, Amped Chess is in the process of building a network of clubs and teams to host regular events and has plans to expand the online platform to include other video and board games.
                     </p>
+                    <a href='https://ampedchess.com'><p  style={{fontWeight:'bold', color:'white',fontSize:14, padding:0, margin:0}}>ampedchess.com</p></a>
                 </div>
 
                 <div class='profile-card' style={{marginTop:20, display:'flex', flexDirection:'column', backgroundColor:'#081424', borderRadius:10, padding:20}}>
@@ -97,19 +95,21 @@ export default function Landing() {
                         <Icon size={80}/>
 
                         <div style={{display:'flex', marginLeft:15, flexDirection:'column', justifyContent:'flexStart'}}>
-                            <p style={{fontWeight:'normal', color:'white', fontSize:16, }} ><b>Amped Trading LLC</b> <br/> Win money playing chess <br/> Esports</p>
+                            <p style={{fontWeight:'normal', color:'white', fontSize:16, }} ><b>Amped Trading</b> <br/> Quantatative trading firm</p>
                         </div>
                     </div>
                         <p style={{color:'white', fontSize:14}}>
                         Amped Trading is comprised of two skilled programmers who specialize in trading crypto assets. We have built a robust infrastructure for blockchain data ingestion, which allows us to calculate 24 real-time metrics of network activity for the Ethereum and Polygon chains. By integrating these metrics with prices across multiple exchanges, we have developed an automated statistical strategy that consistently generates daily to weekly profits.                   
                     </p>
+                    <a href='https://ampedtrading.com'><p  style={{fontWeight:'bold', color:'white',fontSize:14, padding:0, margin:0}}>ampedtrading.com</p></a>
+
                 </div>
 
             
 
-           
+                <div style={{height:50}}/>
 
-                <p style={{marginTop:50,  color:'white', textAlign:'left',  margin:0, padding:0, marginBottom:10,  fontWeight:'bold', fontSize:20}}>Professional Experience</p>
+                <p style={{color:'white', textAlign:'left',  margin:0, padding:0, marginBottom:10,  fontWeight:'bold', fontSize:20}}>Professional Experience</p>
                 
                 <div className={classes.border} />
 
@@ -127,7 +127,9 @@ export default function Landing() {
                     </div>
                 </div>
 
-                <p style={{marginTop:50, textAlign:'left',  margin:0, padding:0, marginBottom:10, color:'white',  fontWeight:'bold', fontSize:20}}>Education</p>
+                <div style={{height:50}}/>
+
+                <p style={{textAlign:'left',  margin:0, padding:0, marginBottom:10, color:'white',  fontWeight:'bold', fontSize:20}}>Education</p>
                 
                 <div className={classes.border} />
 
