bc50dae81ee5d9e9a3923305bcc09250696d6880
david03g <dgoldstein.us@gmail.com>
2023-02-26 16:38:34 -0800

    tweaks

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,6 +7,7 @@ import icon from '../art/icon.png';
 import olem from '../art/olem.png';
 import EmailIcon from '@mui/icons-material/Email';
 import ampedchess from '../art/ampedchess.gif';
+import ampedtrading from '../art/ampedtrading.gif';
 import ordermedia from '../art/ordermedia.jpg';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import ucb from '../art/ucb.jpg';
@@ -58,7 +59,7 @@ export default function Landing() {
                     <p style={{padding:0, margin:0, color:'white',fontSize:14, fontWeight:'bold'}}>davidg3@protonmail.com</p>
                 </div>
 
-                <div style={{display:'flex', marginTop:20, flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+                <div style={{display:'flex', marginTop:10, flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
                     {/* <LocalPhoneIcon style={{color:'white', marginRight:10}}/> */}
                     <p style={{padding:0, margin:0, color:'white', fontSize:14, fontWeight:'bold'}}>+1 (305) 775-2267</p>
                 </div>
@@ -85,30 +86,34 @@ export default function Landing() {
                         </div>
                     </div>
                         <p style={{color:'white', fontSize:14}}>
-                        Both professional sports and esports encounter a common phenomenon known as the "tournament effect," where only a small number of exceptionally skilled or entertaining players can monetize their participation in the sport. Unfortunately, this leaves millions of everyday players with few or no options to compete for prizes. Amped Chess seeks to address this issue by creating a more lucrative environment for the game of chess, primarily through prize pool tournaments and wagers. Currently, Amped Chess is in the process of building a network of clubs and teams to host regular events and has plans to expand the online platform to include other video and board games.
-                    </p>
-                    <a href='https://ampedchess.com'><p  style={{fontWeight:'bold', color:'white',fontSize:14, padding:0, margin:0}}>ampedchess.com</p></a>
+                        Due to the "tournament effect," only a small number of exceptionally skilled or entertaining players can monetize their participation in professional sports/esports. This leaves millions of everyday players with limited or no options to compete for prizes. Amped Chess addresses this issue by creating a more lucrative environment for the game of chess, primarily through prize pool tournaments and wagers. Amped Chess is currently building a network of clubs and teams to host regular events and plans to expand the online platform to include other video/board games.                    </p>
+                    <a style={{textDecoration:'none'}}  href='https://ampedchess.com'><p  style={{fontWeight:'bold', color:'white',fontSize:14, padding:0, margin:0}}>ampedchess.com</p></a>
                 </div>
 
                 <div class='profile-card' style={{marginTop:20, display:'flex', flexDirection:'column', backgroundColor:'#081424', borderRadius:10, padding:20}}>
                     <div style={{display:'flex', flexDirection:'row', alignItems:'center',  borderRadius:10}} >
-                        <Icon size={80}/>
+                        {/* <Icon size={80}/> */}
+                        <Avatar   sx={{ width: 65, height: 65 }} style={{marginLeft:5, marginRight:10}}  src={ampedtrading} />
+
 
                         <div style={{display:'flex', marginLeft:15, flexDirection:'column', justifyContent:'flexStart'}}>
-                            <p style={{fontWeight:'normal', color:'white', fontSize:16, }} ><b>Amped Trading</b> <br/> Quantatative trading firm</p>
+                            <p style={{fontWeight:'normal', color:'white', fontSize:16, }} ><b>Amped Trading</b> <br/> Quantatative Trading Firm</p>
                         </div>
                     </div>
                         <p style={{color:'white', fontSize:14}}>
-                        Amped Trading is comprised of two skilled programmers who specialize in trading crypto assets. We have built a robust infrastructure for blockchain data ingestion, which allows us to calculate 24 real-time metrics of network activity for the Ethereum and Polygon chains. By integrating these metrics with prices across multiple exchanges, we have developed an automated statistical strategy that consistently generates daily to weekly profits.                   
-                    </p>
-                    <a href='https://ampedtrading.com'><p  style={{fontWeight:'bold', color:'white',fontSize:14, padding:0, margin:0}}>ampedtrading.com</p></a>
+                        Amped Trading is composed of two skilled programmers who specialize in trading crypto assets. We have developed a winning automated statistical strategy that consistently generates profits on a daily to weekly basis. Our proprietary robust infrastructure enables us to ingest blockchain data and calculate 24 real-time metrics of Ethereum and Polygon network activity.                        </p>
+                    <a style={{textDecoration:'none'}} href='https://ampedtrading.com'><p  style={{fontWeight:'bold', color:'white',fontSize:14, padding:0, margin:0}}>ampedtrading.com</p></a>
 
                 </div>
+
+
+                
 
             
 
                 <div style={{height:50}}/>
 
+             
                 <p style={{color:'white', textAlign:'left',  margin:0, padding:0, marginBottom:10,  fontWeight:'bold', fontSize:20}}>Professional Experience</p>
                 
                 <div className={classes.border} />
@@ -136,7 +141,7 @@ export default function Landing() {
                 <div style={{marginBottom:40, display:'flex', alignItems:'center', flexDirection:'row', marginTop:10}}>
                     <Avatar  sx={{ width: 70, height: 70 }} src={ucb} />
                     <div style={{display:'flex', marginLeft:15, flexDirection:'column', justifyContent:'flexStart'}}>
-                        <p style={{fontWeight:'normal', fontSize:14, color:'white' }} ><b>University of California, Berkeley</b> <br/> B.S. - Electrical Engineering & Computer Science - Class of '23 <br/>Organizations - Engineers without Borders, Poker / Chess clubs</p>
+                        <p style={{fontWeight:'normal', fontSize:14, color:'white' }} ><b>University of California, Berkeley</b> <br/> B.S. - Electrical Engineering & Computer Science <br/> Class of '23 </p>
                     </div>
                 </div>
 
