20e000bdd311a09bf754ac1eebe808cd4f2dc717
david03g <dgoldstein.us@gmail.com>
2023-03-19 19:19:53 -0700

    order change

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -61,10 +61,9 @@ export default function Landing() {
                     <p style={{padding:0, margin:0, color:'white',fontSize:14, fontWeight:'bold'}}>davidg3@protonmail.com</p>
                 </div>
 
-                <div style={{display:'flex', marginTop:10, flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
-                    {/* <LocalPhoneIcon style={{color:'white', marginRight:10}}/> */}
+                {/* <div style={{display:'flex', marginTop:10, flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
                     <p style={{padding:0, margin:0, color:'white', fontSize:14, fontWeight:'bold'}}>+1 (305) 775-2267</p>
-                </div>
+                </div> */}
 
             </div>
 
@@ -80,7 +79,23 @@ export default function Landing() {
                 </div>
          
 
-                <div class='profile-card' style={{display:'flex', flexDirection:'column', backgroundColor:'#081424', borderRadius:10, padding:20}}>
+                <div class='profile-card' style={{ display:'flex', flexDirection:'column', backgroundColor:'#081424', borderRadius:10, padding:20}}>
+                    <div style={{display:'flex', flexDirection:'row', alignItems:'center',  borderRadius:10}} >
+                        {/* <Icon size={80}/> */}
+                        <Avatar   sx={{ width: 65, height: 65 }} style={{marginLeft:5, marginRight:10}}  src={ampedtrading} />
+
+
+                        <div style={{display:'flex', marginLeft:15, flexDirection:'column', justifyContent:'flexStart'}}>
+                            <p style={{fontWeight:'normal', color:'white', fontSize:16, }} ><b>Amped Trading</b> <br/> Quantatative Trading</p>
+                        </div>
+                    </div>
+                        <p style={{color:'white', fontSize:14}}>
+                        Amped Trading is composed of two skilled programmers who specialize in trading crypto assets. We have developed a winning automated statistical strategy that consistently generates profits on a daily to weekly basis. Our proprietary robust infrastructure enables us to ingest blockchain data and calculate 24 real-time metrics of Ethereum and Polygon network activity.                        </p>
+                    <a style={{textDecoration:'none'}} href='https://ampedtrading.com'><p  style={{fontWeight:'bold', color:'white',fontSize:14, padding:0, margin:0}}>ampedtrading.com</p></a>
+
+                </div>
+
+                <div class='profile-card' style={{marginTop:20, display:'flex', flexDirection:'column', backgroundColor:'#081424', borderRadius:10, padding:20}}>
                     <div style={{display:'flex', flexDirection:'row', alignItems:'center',  borderRadius:10}} >
                         <Avatar   sx={{ width: 80, height: 80 }} src={ampedchess} />
                         <div style={{display:'flex', marginLeft:15, flexDirection:'column', justifyContent:'flexStart'}}>
@@ -93,22 +108,7 @@ export default function Landing() {
                     <a style={{textDecoration:'none'}}  href='https://ampedchess.com'><p  style={{fontWeight:'bold', color:'white',fontSize:14, padding:0, margin:0}}>ampedchess.com</p></a>
                 </div>
 
-                <div class='profile-card' style={{marginTop:20, display:'flex', flexDirection:'column', backgroundColor:'#081424', borderRadius:10, padding:20}}>
-                    <div style={{display:'flex', flexDirection:'row', alignItems:'center',  borderRadius:10}} >
-                        {/* <Icon size={80}/> */}
-                        <Avatar   sx={{ width: 65, height: 65 }} style={{marginLeft:5, marginRight:10}}  src={ampedtrading} />
-
-
-                        <div style={{display:'flex', marginLeft:15, flexDirection:'column', justifyContent:'flexStart'}}>
-                            <p style={{fontWeight:'normal', color:'white', fontSize:16, }} ><b>Amped Trading</b> <br/> Quantatative Trading Firm</p>
-                        </div>
-                    </div>
-                        <p style={{color:'white', fontSize:14}}>
-                        Amped Trading is composed of two skilled programmers who specialize in trading crypto assets. We have developed a winning automated statistical strategy that consistently generates profits on a daily to weekly basis. Our proprietary robust infrastructure enables us to ingest blockchain data and calculate 24 real-time metrics of Ethereum and Polygon network activity.                        </p>
-                    <a style={{textDecoration:'none'}} href='https://ampedtrading.com'><p  style={{fontWeight:'bold', color:'white',fontSize:14, padding:0, margin:0}}>ampedtrading.com</p></a>
-
-                </div>
-
+               
 
                 <div className={classes.container}>
                     <div className={classes.border} />
